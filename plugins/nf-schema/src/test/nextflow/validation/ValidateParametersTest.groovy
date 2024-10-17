38c35fef3f333893b23dfa6f1e9ae0815f148a87
Nicolas Vannieuwkerke <nicolas.vannieuwkerke@ugent.be>
2024-10-17 15:50:05 +0200

    add a new test for non-unique empty values

--- plugins/nf-schema/src/test/nextflow/validation/ValidateParametersTest.groovy
+++ plugins/nf-schema/src/test/nextflow/validation/ValidateParametersTest.groovy
@@ -996,6 +996,31 @@ class ValidateParametersTest extends Dsl2Spec{
         !stdout
     }
 
+    def 'should not fail because of non-unique empty entries' () {
+        given:
+        def schema = Path.of('src/testResources/nextflow_schema_with_samplesheet_uniqueEntries.json').toAbsolutePath().toString()
+        def SCRIPT = """
+            params.input = "src/testResources/samplesheet_non_unique_empty.csv"
+            include { validateParameters } from 'plugin/nf-schema'
+
+            validateParameters(parameters_schema: '$schema')
+        """
+
+        when:
+        def config = ["validation": [
+            "monochromeLogs": true
+        ]]
+        def result = new MockScriptRunner(config).setScript(SCRIPT).execute()
+        def stdout = capture
+                .toString()
+                .readLines()
+                .findResults {it.contains('WARN nextflow.validation.SchemaValidator') || it.startsWith('* --') ? it : null }
+
+        then:
+        noExceptionThrown()
+        !stdout
+    }
+
     def 'should validate nested params - pass' () {
         given:
         def SCRIPT = """
